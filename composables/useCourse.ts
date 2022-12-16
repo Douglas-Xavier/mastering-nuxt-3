fa57ea24918bd5a7a3b46c412942668c2696aaaf
Douglas Xavier <douglasxavier95@gmail.com>
2022-12-16 09:44:31 -0300

    Chapter 3 - Summary

--- composables/useCourse.ts
+++ composables/useCourse.ts
@@ -1,13 +1,43 @@
 import courseData from "./courseData";
-export const useCourse = () => {
+type Lesson = {
+  title: string,
+  slug: string,
+  number: number,
+  downloadUrl: string;
+  videoId: number;
+  text: string;
+  path: string;
+  sourceUrl?: string;
+}
+
+type Chapter = {
+  title: string,
+  slug: string,
+  number: number,
+  lessons: Array<Lesson>,
+}
+
+type Course = {
+  title: string,
+  chapters: Array<Chapter>,
+}
+
+export const useCourse = (): Course => {
+  const chapters: Array<Chapter> = courseData.chapters.map(chapter => {
+
+    const lessons: Array<Lesson> = chapter.lessons.map(lesson => ({
+      ...lesson,
+      path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
+    }));
+
+    return {
+      ...chapter,
+      lessons
+    }
+  })
+
   return {
     ...courseData,
-    chapters: courseData.chapters.map(chapter => ({
-      ...chapter,
-      lessons: chapter.lessons.map(lesson => ({
-        ...lesson,
-        path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
-      }))
-    }))
+    chapters
   }
 }
--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -26,13 +26,40 @@
         />
         <p>{{ lesson.text }}</p>
         <LessonCompleteButton :model-value="isLessonComplete"
-        @update:model-value="toggleComplete"/>
+        @update:model-value="
+        throw createError('Could not update');
+      "/>
       </div>
 </template>
 
 <script setup>
 const course = useCourse();
 const route = useRoute();
+
+definePageMeta({
+  validate({ params }) {
+    const course = useCourse();
+    const chapter = course.chapters.find(
+      (chapter) => chapter.slug === params.chapterSlug
+    );
+    if (!chapter) {
+      return createError({
+        statusCode: 404,
+        message: 'Chapter not found',
+      });
+    }
+    const lesson = chapter.lessons.find(
+      (lesson) => lesson.slug === params.lessonSlug
+    );
+    if (!lesson) {
+      return createError({
+        statusCode: 404,
+        message: 'Lesson not found',
+      });
+    }
+    return true;
+  },
+});
 
 const chapter = computed(() => {
     return course.chapters.find((chapter) => chapter.slug === route.params.chapterSlug)
